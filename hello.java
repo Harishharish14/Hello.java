982c419ebcb70205d0338b1167cabaa6160347ac
harish <harishgodakadi@gmail.com>
2023-12-25 11:49:17 +0530

    changes in hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-Hello {
-	
-	"Hello world"
-	
-}
+    public class Hello {
+	   public static void main( String[] args ) {
+          // Prints "Hello, world" in the terminal window.
+        System.out.println( "Hello, World!" );
+       }
+    }
